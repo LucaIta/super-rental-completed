6c8631b4a6522e489079bcec9cacc818d2af67c2
Ashley Maceli and Daniel Wulff <daniel@>
2016-06-06 16:12:41 -0700

    add delete functionality to announcements

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -8,14 +8,16 @@ export default Ember.Component.extend({
     },
     save1() {
       var params = {
-        owner: this.get('owner') ? this.get('owner') : "",
-        city: this.get('city') ? this.get('city') : "",
-        type: this.get('type') ? this.get('type') : "",
-        image: this.get('image') ? this.get('image') : "",
-        bedrooms: this.get('bedrooms') ? this.get('bedrooms') : ""
+        owner: this.get('owner') ? this.get('owner') : null,
+        city: this.get('city') ? this.get('city') : null,
+        type: this.get('type') ? this.get('type') : null,
+        image: this.get('image') ? this.get('image') : null,
+        bedrooms: this.get('bedrooms') ? this.get('bedrooms') : null
       };
-      this.set('addNewRental', false);
-      this.sendAction('save2', params);
+      if (params.owner !== null ) {
+        this.set('addNewRental', false);
+        this.sendAction('save2', params);
+      }
     }
   }
 });
--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return (this.store.findAll('announcement'));
+  },
+  actions: {
+    delete(announcement) {
+      if (confirm('Are you sure you want to delete this announcement?')) {
+          announcement.destroyRecord();
+      }
+    }
   }
 });
